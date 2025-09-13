450715ae99f9086bee13b468bcb80c12691472b7
Софья <kiratuber01@gmail.com>
2025-09-13 10:29:30 +0300

    задание 2

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -1,7 +1,7 @@
 package me.liquor4k;
 
 //задание 1
-public class VariableDeclaration {
+public class Main {
     public static void main(String[] args) {
         // Объявление переменных
         int myNumber = 42;
@@ -18,5 +18,25 @@ public class VariableDeclaration {
         System.out.println("Значение переменной bigNumber с типом long равно " + bigNumber);
         System.out.println("Значение переменной decimalNumber с типом float равно " + decimalNumber);
         System.out.println("Значение переменной preciseNumber с типом double равно " + preciseNumber);
+
+        //задание 2
+
+        // Инициализация переменных с указанными значениями
+        float firstValue = 27.12f;          // float для числа с плавающей точкой
+        long secondValue = 987678965549L;   // long для большого целого числа
+        double thirdValue = 2.786;          // double для числа с плавающей точкой
+        short fourthValue = 569;            // short для целого числа
+        short fifthValue = -159;            // short для отрицательного целого числа
+        int sixthValue = 27897;             // int для целого числа
+        byte seventhValue = 67;             // byte для маленького целого числа
+
+        // Вывод значений в консоль
+        System.out.println("Значение переменной firstValue с типом float равно " + firstValue);
+        System.out.println("Значение переменной secondValue с типом long равно " + secondValue);
+        System.out.println("Значение переменной thirdValue с типом double равно " + thirdValue);
+        System.out.println("Значение переменной fourthValue с типом short равно " + fourthValue);
+        System.out.println("Значение переменной fifthValue с типом short равно " + fifthValue);
+        System.out.println("Значение переменной sixthValue с типом int равно " + sixthValue);
+        System.out.println("Значение переменной seventhValue с типом byte равно " + seventhValue);
     }
 }
